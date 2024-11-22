dfabcdebaf61bc35350c71bce4512d8921be7f4a
BangGhani <armynabigh@gmail.com>
2024-11-22 10:26:50 +0700

    Tambah fitur hapus data

--- lib/deleteData.dart
+++ lib/deleteData.dart
@@ -1,6 +1,5 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> deleteBook(int bookId) async {
-  final response =
-      await Supabase.instance.client.from('books').delete().eq('id', bookId);
+  await Supabase.instance.client.from('books').delete().eq('id', bookId); //hapus data
 }
